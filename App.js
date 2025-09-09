5600ad7cccf9ffc2710a0b070d864fe9b040de74
punambora <punam@thecloudfountain.com>
2025-09-09 22:30:35 +0530

    Episode 2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 let parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am an h1 tag"),
@@ -8,5 +11,5 @@ let parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I am an h2 tag"),
   ]),
 ]);
-let root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
