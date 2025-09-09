01aeeae0e52f0b8e118a00dc48422dcc71464091
punambora <punam@thecloudfountain.com>
2025-09-09 09:12:40 +0530

    Created complex react structure

---
+++ App.js
@@ -0,0 +1,12 @@
+let parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { className: "header" }, "I am an h1 tag"),
+    React.createElement("h2", {}, "I am an h2 tag"),
+  ]),
+]);
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
