4cc4edbe5a7994ef3b0366997aeee3212eb1179c
punambora <punam@thecloudfountain.com>
2025-09-14 09:51:48 +0530

    Implemented routing

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <h2>This is Punam Bora learning react from Namaste React web Series</h2>
+    </div>
+  );
+};
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom/client";
 import Header from "./Header";
-import Body from "./Body";
+import { Body } from "./Body";
+import About from "./About";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Contact from "./Contact";
 
 const OrderAppComponent = () => {
   return (
@@ -11,5 +14,20 @@ const OrderAppComponent = () => {
   );
 };
 
+let appRouter = createBrowserRouter([
+  {
+    path: "/",
+    Component: OrderAppComponent,
+  },
+  {
+    path: "/about",
+    Component: About,
+  },
+  {
+    path: "/contact",
+    Component: Contact,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(OrderAppComponent());
+root.render(<RouterProvider router={appRouter} />);
